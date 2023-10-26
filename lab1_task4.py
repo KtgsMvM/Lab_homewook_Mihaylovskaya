0b3fe246f85ab53776d6a8880f69b0abb618d34d
KtgsMvM <147740686+KtgsMvM@users.noreply.github.com>
2023-10-26 18:29:41 +0300

    Delete lab1_task4.py

--- lab1_task4.py
+++
@@ -1,7 +0,0 @@
-users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
-dict_ ={"Общее количество": 0, "Уникальные посещения": 0}
-users_= len(users)
-users_1 = len(set(users))
-dict_new={"Общее количество": users_, "Уникальные посещения": users_1}
-print(dict_new)
-# TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
